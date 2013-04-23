4c329d1f2b6982d7132d317d018364f0adc36d5c
Tola Oredope <tolaoredope@gmail.com>
2013-04-23 14:11:14 +0100

    Clean Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Asoebi::Application.config.secret_key_base = 'aeafd82306b970ce6828e39e1f836f448f49d7c33cf4892261d65713fb3e24f61c92e6877f93bdf82a42b8cb0b3ce82d4a26d030f071e478b17f4a2e644cba5c'
