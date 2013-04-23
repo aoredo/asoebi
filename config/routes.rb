6b539cd208038689ca86f522460008a9d08a4261
Tola Oredope <tolaoredope@gmail.com>
2013-04-23 17:01:02 +0100

    Added Static Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Asoebi::Application.routes.draw do
+  get "static/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -46,4 +47,6 @@ Asoebi::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root to: 'static#index'
 end
