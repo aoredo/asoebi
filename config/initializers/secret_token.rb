9b6027a90a2eafec411b6779e3647098b1c7b716
Tola Oredope <tolaoredope@gmail.com>
2013-04-24 01:37:00 +0100

    Downgrade Rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,11 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Asoebi::Application.config.secret_key_base = 'aeafd82306b970ce6828e39e1f836f448f49d7c33cf4892261d65713fb3e24f61c92e6877f93bdf82a42b8cb0b3ce82d4a26d030f071e478b17f4a2e644cba5c'
+Asoebi::Application.config.secret_token = '7262925f676cef58a24bd554af207b3c12925f5999c67e09ebbad9558cf66c2335a7e03289b0915fb277dda50744e490a06c3147031f8f39a45f3d7dfdf86bf6'
